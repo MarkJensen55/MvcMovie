c7a57780d86898c5be3b761c33953b330c413ae7
MarkJensen55 <markjensen@tds.net>
2015-04-01 11:14:07 -0400

    Changed date attributes for local format

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -11,7 +11,7 @@ namespace MvcMovie.Models
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString= "{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+        [DisplayFormat(DataFormatString= "{0:d}", ApplyFormatInEditMode=true)]
         public DateTime ReleaseDate { get; set; }
 
         public string Genre { get; set; }
