8204a963ff0a2ec66fcc787e47a6a061c4a3358e
MarkJensen55 <markjensen@tds.net>
2015-04-03 15:21:00 -0400

    more attributes

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,7 +7,8 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
-
+        
+        // wow 0 is greater than 3!!
         [StringLength(60, MinimumLength=3)]
         public string Title { get; set; }
 
@@ -17,8 +18,17 @@ namespace MvcMovie.Models
         [DisplayFormat(DataFormatString= "{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
         public DateTime ReleaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[A-Za-z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[A-Za-z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
